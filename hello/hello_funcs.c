c075a0dfb5def59773b7a41423d999f946dd097f
Michael James <micrjames921@gmail.com>
2023-03-20 06:18:19 -0700

    removed a line from 'hello_funcs.c'

--- hello/hello_funcs.c
+++ hello/hello_funcs.c
@@ -9,7 +9,5 @@ unsigned long ask(char **message) {
    unsigned long len;                                                     
    printf("%s", "What would you like to say? ");
 
-   unsigned long read = getline(message, &len, stdin);
-
-   return read;
+   return getline(message, &len, stdin);
 }
